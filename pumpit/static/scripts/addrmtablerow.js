4ba0990720c3f270aa049d22996d60028dfce9f3
Bruno Kiyoshi <ynumaru@gmail.com>
2022-04-17 23:05:29 -0300

    create fittings table and add interactivities to it

--- pumpit/static/scripts/addrmtablerow.js
+++ pumpit/static/scripts/addrmtablerow.js
@@ -1,90 +1,84 @@
-const $tableID = $('#table');
-const $BTN = $('#export-btn');
-const $EXPORT = $('#export');
+const tableIDs = ['#pipingtable', "#fittingstable"];
+const tableAddIDs = ["#table-add-piping", "#table-add-fittings"];
 
-//TODO: FIND OUT HOW TO GET THE STRING FROM stdtablerow.html
+// TODO: use better data structure instead of relying on indexes matching
+for (let i = 0; i < tableIDs.length; i++) {
+    const $tableID = $(tableIDs[i]);
+    const $BTN = $('#export-btn');
+    const $EXPORT = $('#export');
 
-// const newTr = fetch('/getstdtablerow').then(function (response) {
-// 	// The API call was successful!
-// 	return response.text();
-// }).then(function (html) {
-//
-// 	// Convert the HTML string into a document object
-// 	var parser = new DOMParser();
-// 	var doc = parser.parseFromString(html, 'text/html');
-//
-// }).catch(function (err) {
-// 	// There was an error
-// 	console.warn('Something went wrong.', err);
-// });
+    const newTr = '<tr></tr>' // TODO: find way to get standard row from html files
+    // TODO: use better data structure instead of relying on indexes matching
+    $(tableAddIDs[i]).on('click', 'i', () => {
+        const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
 
-const newTr = '<tr></tr>'
+        if ($tableID.find('tbody tr').length === 0) {
 
-$('.table-add').on('click', 'i', () => {
+            $('tbody').append(newTr);
+        }
 
-    const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
+        $tableID.find('table').append($clone);
+    });
 
-    if ($tableID.find('tbody tr').length === 0) {
+    $tableID.on('click', '.table-remove', function () {
+        if (i===0){
+            if ($tableID.find('tbody tr').length > 1) {
+                $(this).parents('tr').detach();
+            }
+        }else {
+            $(this).parents('tr').detach();
+        }
 
-        $('tbody').append(newTr);
-    }
+    });
 
-    $tableID.find('table').append($clone);
-});
+    $tableID.on('click', '.table-up', function () {
 
-$tableID.on('click', '.table-remove', function () {
-    if ($tableID.find('tbody tr').length > 1) {
-        $(this).parents('tr').detach();
-    }
-});
+        const $row = $(this).parents('tr');
 
-$tableID.on('click', '.table-up', function () {
+        if ($row.index() === 0) {
+            return;
+        }
 
-    const $row = $(this).parents('tr');
+        $row.prev().before($row.get(0));
+    });
 
-    if ($row.index() === 0) {
-        return;
-    }
+    $tableID.on('click', '.table-down', function () {
 
-    $row.prev().before($row.get(0));
-});
-
-$tableID.on('click', '.table-down', function () {
-
-    const $row = $(this).parents('tr');
-    $row.next().after($row.get(0));
-});
+        const $row = $(this).parents('tr');
+        $row.next().after($row.get(0));
+    });
 
 // A few jQuery helpers for exporting only
-jQuery.fn.pop = [].pop;
-jQuery.fn.shift = [].shift;
+    jQuery.fn.pop = [].pop;
+    jQuery.fn.shift = [].shift;
 
-$BTN.on('click', () => {
+    $BTN.on('click', () => {
 
-    const $rows = $tableID.find('tr:not(:hidden)');
-    const headers = [];
-    const data = [];
+        const $rows = $tableID.find('tr:not(:hidden)');
+        const headers = [];
+        const data = [];
 
-    // Get the headers (add special header logic here)
-    $($rows.shift()).find('th:not(:empty)').each(function () {
+        // Get the headers (add special header logic here)
+        $($rows.shift()).find('th:not(:empty)').each(function () {
 
-        headers.push($(this).text().toLowerCase());
-    });
-
-    // Turn all existing rows into a loopable array
-    $rows.each(function () {
-        const $td = $(this).find('td');
-        const h = {};
-
-        // Use the headers from earlier to name our hash keys
-        headers.forEach((header, i) => {
-
-            h[header] = $td.eq(i).text();
+            headers.push($(this).text().toLowerCase());
         });
 
-        data.push(h);
-    });
+        // Turn all existing rows into a loopable array
+        $rows.each(function () {
+            const $td = $(this).find('td');
+            const h = {};
 
-    // Output the result
-    $EXPORT.text(JSON.stringify(data));
-});
+            // Use the headers from earlier to name our hash keys
+            headers.forEach((header, i) => {
+
+                h[header] = $td.eq(i).text();
+            });
+
+            data.push(h);
+        });
+
+        // Output the result
+        $EXPORT.text(JSON.stringify(data));
+    });
+}
